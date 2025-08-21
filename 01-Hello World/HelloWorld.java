9fa026743ecdca6f28be78974c722eb70f389d50
j24043053-Nanda <j24043053@student.newinti.edu.my>
2025-08-21 13:50:39 +0800

    Update HelloWorld.java

--- 01-Hello World/HelloWorld.java
+++ 01-Hello World/HelloWorld.java
@@ -7,6 +7,9 @@ public class HelloWorld {
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
     {
-        System.out.println("Hello, World");
+        System.out.println("My name is Nanda Kyaw");
+        System.out.println("Course Name: Object-Orientd Programming in Java I");
+        System.out.println("Course code: CSC200");
+        System.out.println("Lecturer Name: Sathish Sarma");
     }
 }
